b63d43eb9686b59a7c8dead0218b0d9ca629dbc5
Zeta-Naidi <znaidi2003@gmail.com>
2024-03-01 18:15:02 +0100

    main changes

--- src/globalServices/translate.js
+++
@@ -1,6 +0,0 @@
-import languages from './langs.json'
-class Translate {
-     getTranslation(language,key) {
-        return !!language ? languages[language][key] : key;
-    }
-} export default new Translate()
--- src/main.js
+++ src/main.js
@@ -1,42 +1,17 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-const start = window.performance.now();
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import App from '@/App.vue'
+import { registerPlugins } from '@core/utils/plugins'
 
+// Styles
+import '@core/scss/template/index.scss'
+import '@layouts/styles/index.scss'
+
+// Create vue app
 const app = createApp(App)
 
 
-//Pinia
-import pinia from "@/plugins/pinia";
-
-
-import utilsFunctions from "@/globalServices/utilsFunctions";
-
-// Restore state after reload
-const savedState = sessionStorage.getItem("piniaAppState");
-if (savedState) {
-    pinia.state.value = JSON.parse(savedState);
-}
-
-// Save state before unload
-window.addEventListener("beforeunload", () => {
-    sessionStorage.setItem("piniaAppState", JSON.stringify(pinia.state.value));
-});
-
+// Register plugins
 registerPlugins(app)
-app.mount("#app");
 
-const end = window.performance.now();
-const duration = end - start;
-utilsFunctions.appReady(duration);
+// Mount vue app
+app.mount('#app')
